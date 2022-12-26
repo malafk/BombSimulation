5e7c82eaf2e75a07abce6c4ce5f6517e44f7e713
Mal <34745578+malafk@users.noreply.github.com>
2022-12-26 22:41:40 +0000

    Start Project

---
+++ src/main/java/lol/maltest/bombsimulation/BombSimulation.java
@@ -0,0 +1,17 @@
+package lol.maltest.bombsimulation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BombSimulation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
